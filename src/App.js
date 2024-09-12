3abeadae529c86327b1f5c8790a416b23b845805
Alex990407 <zhelezniako07@gmail.com>
2024-09-12 13:52:29 +0200

    last commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import React from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
 import Home from "./components/Home/indes";
-import ArticleList from "./components/ArticleList";
+import ArticleList from "./components/ArticleList/index";
 import Article from "./components/Article";
 import styles from "./components/styles/App.module.css";
 
 function App() {
   return (
-    <div className={styles.container}>
-      <nav>
+    <div className={styles.appContainer}>
+      {/* Навигация между страницами */}
+      <nav className={styles.nav}>
         <ul>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/">Home</NavLink> {/* Ссылка на главную страницу */}
           </li>
           <li>
-            <NavLink to="/articles">Articles</NavLink>
+            <NavLink to="/articles">Articles</NavLink>{" "}
+            {/* Ссылка на список статей */}
           </li>
         </ul>
       </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/articles" element={<ArticleList />} />
-        <Route path="/article/:id" element={<Article />} />
-      </Routes>
+      <div className={styles.contentContainer}>
+        {/* Удален прямой вызов ArticleList, чтобы избежать дублирования */}
+        {/* Вместо этого используем маршрутизацию для рендеринга ArticleList */}
+
+        <Routes>
+          {/* Маршрут для главной страницы */}
+          <Route path="/" element={<Home />} />
+
+          {/* Маршрут для списка статей */}
+          <Route path="/articles" element={<ArticleList />} />
+          {/* Компонент ArticleList больше не рендерится дважды */}
+
+          {/* Маршрут для отдельной статьи */}
+          <Route path="/article/:id" element={<Article />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/ArticleList/index.jsx
+++ src/components/ArticleList/index.jsx
@@ -24,3 +24,31 @@ const ArticleList = () => {
 };
 
 export default ArticleList;
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import styles from "./styles/styles.module.css"; // Подключение стилей
+
+// const articles = [
+//   { id: 1, title: "Article 1" },
+//   { id: 2, title: "Article 2" },
+//   { id: 3, title: "Article 3" },
+// ];
+
+// function ArticleList() {
+//   return (
+//     <div>
+//       <h1>Articles</h1>
+//       <ul>
+//         {articles.map((article) => (
+//           <li className={styles.li} key={article.id}>
+//             {/* Ссылки на статьи с помощью Link */}
+//             <Link to={`/article/${article.id}`}>{article.title}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ArticleList;
